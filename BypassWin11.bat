e005a63d923873fc6602a7a50cef98f55e1e59e2
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2021-08-16 18:06:08 +0100

    Update BypassWin11.bat

--- BypassWin11.bat
+++ BypassWin11.bat
@@ -36,6 +36,8 @@ if %input% == 0 cls && goto :end
 goto :eof
 
 :bypass
+echo Bypass Windows 11 System Requirements
+echo.
 echo  * Bypass CPU Check
 echo  * Bypass Storage Check
 echo  * Bypass RAM Check
@@ -51,6 +53,7 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /f /t
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /f /t REG_DWORD /d 00000001
 echo Go back to previous back and then go to the next page to proceed.
 echo The "Unsupported" screen should no longer appear and you can continue the installation.
+echo.
 pause && cls & goto :main
 goto :eof
 
