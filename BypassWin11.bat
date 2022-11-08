4b25d9d837d32255da86a06a9a27704d5ac450d2
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2022-11-08 19:55:04 +0100

    Update BypassWin11.bat

--- BypassWin11.bat
+++ BypassWin11.bat
@@ -72,7 +72,10 @@ pause && cls && goto :main
 goto :eof
 
 :sysinfo
+echo System Information
+echo.
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"OS Manufacturer" /C:"OS Configuration" /C:"OS Build Type" /C:"Original Install Date" /C:"System Boot Time" /C:"System Manufacturer" /C:"System Model" /C:"System Type" /C:"Processor(s)" /C:"BIOS Version" /C:"Windows Directory" /C:"System Directory" /C:"Boot Device" /C:"System Locale" /C:"Input Locale" /C:"Total Physical Memory" /C:"Available Physical Memory" /C:"Virtual Memory: Max Size" /C:"Virtual Memory: Available" /C:"Virtual Memory: In Use" /C:"Domain" /C:"Network Card(s)"
+echo.
 pause && cls && goto :main
 goto :eof
 
