96b9fc1218eedda567413972a819ace4c0bdffc9
Jory Aouati <117789028+joryaouati@users.noreply.github.com>
2022-11-09 03:46:08 +0100

    Update BypassWin11.bat

--- BypassWin11.bat
+++ BypassWin11.bat
@@ -4,7 +4,7 @@ mode 80,25
 color 07
 REM Bypass Windows 11 System Requirements
 REM Author: Haitham Aouati & Jory Aouati
-REM Latest update: 08/11/2022
+REM Latest update: 09/11/2022
 cls
 
 goto :elevation
