cb1e43b2f93e5458a54ae16ec56400bbe2102386
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2022-11-14 18:53:37 +0100

    Update BypassWin11.bat

--- BypassWin11.bat
+++ BypassWin11.bat
@@ -2,9 +2,8 @@
 title Bypass Windows 11
 mode 80,25
 color 07
-REM Bypass Windows 11 System Requirements
 REM Author: Haitham Aouati
-REM Latest update: 09/11/2022
+REM GitHub: https://github.com/haithamaouati
 cls
 
 goto :elevation
@@ -38,14 +37,12 @@ echo  Author: Haitham Aouati
 echo  Repo: https://github.com/haithamaouati/BypassWin11
 echo.
 echo 1) Bypass Requirements
-echo 2) System Information
 echo.
 echo 0) Exit
 echo.
 set /p input=Choice:
 if %input% gtr 1 cls && goto :main
 if %input% == 1 cls && goto :bypass
-if %input% == 2 cls && goto :sysinfo
 if %input% == 0 cls && goto :end
 goto :eof
 
@@ -67,14 +64,6 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /f /t
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /f /t REG_DWORD /d 00000001
 echo Go back to previous back and then go to the next page to proceed.
 echo The "Unsupported" screen should no longer appear and you can continue the installation.
-echo.
-pause && cls && goto :main
-goto :eof
-
-:sysinfo
-echo System Information
-echo.
-systeminfo | findstr /B /C:"OS Name" /C:"OS Version" /C:"OS Manufacturer" /C:"OS Configuration" /C:"OS Build Type" /C:"Original Install Date" /C:"System Boot Time" /C:"System Manufacturer" /C:"System Model" /C:"System Type" /C:"Processor(s)" /C:"BIOS Version" /C:"Windows Directory" /C:"System Directory" /C:"Boot Device" /C:"System Locale" /C:"Input Locale" /C:"Total Physical Memory" /C:"Available Physical Memory" /C:"Virtual Memory: Max Size" /C:"Virtual Memory: Available" /C:"Virtual Memory: In Use" /C:"Domain" /C:"Network Card(s)"
 echo.
 pause && cls && goto :main
 goto :eof
