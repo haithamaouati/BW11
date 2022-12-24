3937f8ee4c2d69161d70e394f9029e21bc64a6bd
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2022-12-24 01:56:12 +0100

    Update BypassWin11.bat

--- BypassWin11.bat
+++ BypassWin11.bat
@@ -1,25 +1,26 @@
 @echo off
-title Bypass Windows 11
+title BypassWin11
 mode 80,25
 color 07
+
+REM BypassWin11
+REM Version: 1.0
+REM Description: A script for bypassing minimum system requirements to installing Windows 11.
 REM Author: Haitham Aouati
-REM GitHub: https://github.com/haithamaouati
+REM GitHub: https://github.com/haithamaouati/BypassWin11
+
 cls
 
-goto :elevation
-
-:elevation
 net session >nul 2>&1
-if %errorLevel% == 0 (
-cls && goto :main
+if %errorlevel% == 0 (
+    goto main_menu
 ) else (
-echo Run as administrator.
-pause >nul && exit
+    echo This script must be run as an administrator.
+    pause
+    exit
 )
-goto :eof
 
-:main
-echo BypassWin11 - Bypass Windows 11 System Requirements
+:main_menu
 echo.
 echo  ##########  ##########
 echo  ##########  ##########
@@ -33,28 +34,24 @@ echo  ##########  ##########
 echo  ##########  ##########
 echo  ##########  ##########
 echo.
-echo  Author: Haitham Aouati
-echo  Repo: https://github.com/haithamaouati/BypassWin11
+echo Main menu:
+echo 1. Bypass
+echo 2. Exit
 echo.
-echo 1) Bypass Requirements
-echo.
-echo 0) Exit
-echo.
-set /p input=Choice:
-if %input% gtr 1 cls && goto :main
-if %input% == 1 cls && goto :bypass
-if %input% == 0 cls && goto :end
-goto :eof
+
+set /p choice=Enter your choice: 
+
+if "%choice%"=="1" goto bypass
+if "%choice%"=="2" goto end
 
 :bypass
-echo Bypass Windows 11 System Requirements
-echo.
-echo  * Bypass CPU Check
-echo  * Bypass Storage Check
-echo  * Bypass RAM Check
-echo  * Bypass TPM Check
-echo  * Bypass SecureBoot Check
-echo  * Allow Upgrade With Unsupported TPM or CPU
+echo This section allows you to bypass various checks.
+echo 1. Bypass CPU Check
+echo 2. Bypass Storage Check
+echo 3. Bypass RAM Check
+echo 4. Bypass TPM Check
+echo 5. Bypass SecureBoot Check
+echo 6. Allow Upgrade With Unsupported TPM or CPU
 echo.
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassCPUCheck /f /t REG_DWORD /d 00000001
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassStorageCheck /f /t REG_DWORD /d 00000001
@@ -65,11 +62,12 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedT
 echo Go back to previous back and then go to the next page to proceed.
 echo The "Unsupported" screen should no longer appear and you can continue the installation.
 echo.
-pause && cls && goto :main
-goto :eof
+
+pause
+goto main_menu
 
 :end
-choice /c YN /n /t 10 /d Y /m "Do you want exit the program (Y/N)?"
-if %errorlevel% equ 1 exit /b
-if %errorlevel% equ 2 cls && goto :main
-goto :eof
+echo Are you sure you want to exit the script? (y/n)
+set /p confirm=
+if /i "%confirm%"=="y" exit
+if /i "%confirm%"=="n" goto main_menu
