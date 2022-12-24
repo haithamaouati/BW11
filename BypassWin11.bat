ca30b94709c17ab7be47d92739f66504f5a6ba35
Haitham Aouati <55059470+haithamaouati@users.noreply.github.com>
2022-12-24 02:04:24 +0100

    Update BypassWin11.bat

--- BypassWin11.bat
+++ BypassWin11.bat
@@ -3,12 +3,6 @@ title BypassWin11
 mode 80,25
 color 07
 
-REM BypassWin11
-REM Version: 1.0
-REM Description: A script for bypassing minimum system requirements to installing Windows 11.
-REM Author: Haitham Aouati
-REM GitHub: https://github.com/haithamaouati/BypassWin11
-
 cls
 
 net session >nul 2>&1
@@ -21,22 +15,9 @@ if %errorlevel% == 0 (
 )
 
 :main_menu
-echo.
-echo  ##########  ##########
-echo  ##########  ##########
-echo  ##########  ##########
-echo  ##########  ##########
-echo  ##########  ##########
-echo.
-echo  ##########  ##########
-echo  ##########  ##########
-echo  ##########  ##########
-echo  ##########  ##########
-echo  ##########  ##########
-echo.
 echo Main menu:
 echo 1. Bypass
-echo 2. Exit
+echo 2. End
 echo.
 
 set /p choice=Enter your choice: 
@@ -53,16 +34,16 @@ echo 4. Bypass TPM Check
 echo 5. Bypass SecureBoot Check
 echo 6. Allow Upgrade With Unsupported TPM or CPU
 echo.
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassCPUCheck /f /t REG_DWORD /d 00000001
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassStorageCheck /f /t REG_DWORD /d 00000001
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassRAMCheck /f /t REG_DWORD /d 00000001
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassTPMCheck /f /t REG_DWORD /d 00000001
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /f /t REG_DWORD /d 00000001
-reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /f /t REG_DWORD /d 00000001
-echo Go back to previous back and then go to the next page to proceed.
-echo The "Unsupported" screen should no longer appear and you can continue the installation.
-echo.
 
+echo Adding keys to bypass various checks...
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Windows 11\Update" /v CPUCheck /t REG_SZ /d 0 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Windows 11\Update" /v StorageCheck /t REG_SZ /d 0 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Windows 11\Update" /v RAMCheck /t REG_SZ /d 0 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Windows 11\Update" /v TPMCheck /t REG_SZ /d 0 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Windows 11\Update" /v SecureBootCheck /t REG_SZ /d 0 /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Windows 11\Update" /v UnsupportedTPMorCPU /t REG_SZ /d 0 /f
+
+echo Keys added successfully.
 pause
 goto main_menu
 
